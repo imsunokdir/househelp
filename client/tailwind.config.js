1c4ecde1d059c4397a89baf4732e22deebfcc2be
imsunokdir <67774734+imsunokdir@users.noreply.github.com>
2024-12-14 16:00:03 +0530

    change node version in package.json

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,9 +1,17 @@
-const withMT = require("@material-tailwind/react/utils/withMT");
 /** @type {import('tailwindcss').Config} */
-export default withMT({
+export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-});
+};
+// const withMT = require("@material-tailwind/react/utils/withMT");
+// /** @type {import('tailwindcss').Config} */
+// export default withMT({
+//   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// });
