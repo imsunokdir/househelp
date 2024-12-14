cb2495441d0081e106e0556a6366757f4d1244cb
imsunokdir <67774734+imsunokdir@users.noreply.github.com>
2024-12-14 17:21:56 +0530

    change vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,5 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,12 +11,12 @@ export default defineConfig({
   server: {
     proxy: {
       "/auth": {
-        target: import.meta.env.VITE_ROOT_ROUTE,
+        target: process.env.VITE_ROOT_ROUTE,
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/auth/, ""), // Optional path rewrite
       },
       "/api": {
-        target: import.meta.env.VITE_ROOT_ROUTE, // Backend server
+        target: process.env.VITE_ROOT_ROUTE, // Backend server
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""), // Adjust backend paths if necessary
       },
