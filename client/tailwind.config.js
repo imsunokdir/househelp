a9795f8f77e62703d6fc33b2c9744305747a5b95
imsunokdir <67774734+imsunokdir@users.noreply.github.com>
2024-12-14 17:03:03 +0530

    change taiwlind config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,17 +1,17 @@
-// /** @type {import('tailwindcss').Config} */
-// export default {
-//   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-//   theme: {
-//     extend: {},
-//   },
-//   plugins: [],
-// };
-const withMT = require("@material-tailwind/react/utils/withMT");
 /** @type {import('tailwindcss').Config} */
-export default withMT({
+export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-});
+};
+// const withMT = require("@material-tailwind/react/utils/withMT");
+// /** @type {import('tailwindcss').Config} */
+// export default withMT({
+//   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// });
