e4f479529aa5154b7445e73be3fdf36c4ac3eba8
imsunokdir <67774734+imsunokdir@users.noreply.github.com>
2024-12-14 20:29:21 +0530

    configuring cross origin

--- server/server.js
+++ server/server.js
@@ -55,7 +55,7 @@ app.use(
     saveUninitialized: false,
     cookie: {
       httpOnly: true,
-      secure: true, // Set true if using HTTPS
+      secure: process.env.NODE_ENV === "production", // Set true if using HTTPS
       maxAge: 24 * 60 * 60 * 1000,
       sameSite: "none",
     },
