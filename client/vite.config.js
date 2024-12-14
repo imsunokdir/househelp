be1ec4445ce25312491284de8ed0d65d3c5ea47f
imsunokdir <67774734+imsunokdir@users.noreply.github.com>
2024-12-14 16:04:20 +0530

    react cookie error

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import withMT from "@material-tailwind/react/utils/withMT";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: [
+        "@material-tailwind/react", // Externalize the material-tailwind package
+      ],
+    },
+  },
   // server: {
   //   proxy: {
   //     "/auth": {
