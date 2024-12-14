ebf24fbd2fb52944d5a31822e37e475df754df5a
imsunokdir <67774734+imsunokdir@users.noreply.github.com>
2024-12-14 15:40:11 +0530

    edit/change vercel configs

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,18 +4,18 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/auth": {
-        target: import.meta.env.VITE_ROOT_ROUTE,
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/auth/, ""), // Optional path rewrite
-      },
-      "/api": {
-        target: import.meta.env.VITE_ROOT_ROUTE, // Backend server
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""), // Adjust backend paths if necessary
-      },
-    },
-  },
+  // server: {
+  //   proxy: {
+  //     "/auth": {
+  //       target: import.meta.env.VITE_ROOT_ROUTE,
+  //       changeOrigin: true,
+  //       rewrite: (path) => path.replace(/^\/auth/, ""), // Optional path rewrite
+  //     },
+  //     "/api": {
+  //       target: import.meta.env.VITE_ROOT_ROUTE, // Backend server
+  //       changeOrigin: true,
+  //       rewrite: (path) => path.replace(/^\/api/, ""), // Adjust backend paths if necessary
+  //     },
+  //   },
+  // },
 });
