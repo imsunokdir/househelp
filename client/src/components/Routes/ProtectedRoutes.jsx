bf36e31c8145fb7f3c1ec04c1681fb81d4258da4
imsunokdir <67774734+imsunokdir@users.noreply.github.com>
2024-12-17 16:09:01 +0530

    google dynamic redirect

--- client/src/components/Routes/ProtectedRoutes.jsx
+++ client/src/components/Routes/ProtectedRoutes.jsx
@@ -3,27 +3,26 @@ import { AuthContext } from "../../contexts/AuthProvider";
 import { Navigate, Outlet, useLocation } from "react-router-dom";
 import LoadBalls from "../LoadingSkeleton/LoadBalls";
 
-const ProtectedRoutes = ({ children }) => {
+const ProtectedRoutes = () => {
   const { isAuth, authLoading } = useContext(AuthContext);
   const location = useLocation();
+
   console.log("isAuth:", isAuth);
 
-  {
-    authLoading ? (
-      <LoadBalls />
-    ) : !isAuth ? (
+  // If loading, show the loading indicator
+  if (authLoading) {
+    return <LoadBalls />;
+  }
+
+  // If not authenticated, redirect to login
+  if (!isAuth) {
+    return (
       <Navigate to="/user-auth/login" state={{ from: location }} replace />
-    ) : (
-      <Outlet />
     );
   }
 
-  // if (!isAuth) {
-  //   return (
-  //     <Navigate to="/user-auth/login" state={{ from: location }} replace />
-  //   );
-  // }
-  // return <Outlet />;
+  // If authenticated, render the protected routes
+  return <Outlet />;
 };
 
 export default ProtectedRoutes;
