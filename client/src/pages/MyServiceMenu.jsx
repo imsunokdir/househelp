48a50878cfe44ebc08396da06db1d2655ddb5102
imsunokdir <67774734+imsunokdir@users.noreply.github.com>
2025-04-04 17:14:47 +0530

    fixed padding in add service form in accounts

--- client/src/pages/MyServiceMenu.jsx
+++ client/src/pages/MyServiceMenu.jsx
@@ -29,7 +29,7 @@ const MyServiceMenu = () => {
     );
 
   return (
-    <div className="p-4">
+    <div className="p-0">
       {/* Show menu only when NOT in hideMenuRoutes */}
       {!isHidden && (
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3">
